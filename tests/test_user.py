8005b2ecb65ac7537f113dc3f8706c5d178ee0d0
choitaehoon1998 <hugebigdream@gmail.com>
2020-03-02 20:53:36 +0900

    Remove Unrelated file

--- tests/test_user.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from user.models import User
-
-
-@pytest.mark.django_db
-def test_user_create():
-    User.objects.create_user('hugebigdream@gmail.com', "choi", "taehoon", "1111")
-    assert User.objects.count() == 1
