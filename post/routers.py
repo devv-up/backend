1d2f3b208fcf0fb9f54e2abc1af7a395be70c178
yimsb01 <yimsb02@naver.com>
2020-03-06 03:10:09 +0900

    Add routers from the post app

---
+++ post/routers.py
@@ -0,0 +1,25 @@
+"""dev_up URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+
+from post.api.category import CategoryAPI, CategoryList, CategoryPosting
+
+urlpatterns = [
+    path('category/', CategoryPosting.as_view()),
+    path('category/<int:category_id>/', CategoryAPI.as_view()),
+    path('categories/', CategoryList.as_view()),
+]
