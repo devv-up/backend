4966f3baf04f9e688c92216035ccddf2bf91c22f
yimsb01 <yimsb02@naver.com>
2020-03-06 19:37:48 +0900

    Implement a tag serializer

--- post/serializers.py
+++ post/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers  # type: ignore
 
-from post.models import Category
+from post.models import Category, Tag
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        fields = ('id', 'title')
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
         fields = ('id', 'title')
