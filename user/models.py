61fd780903da86dfc5e3215262660ce625dc43a2
choitaehoon1998 <hugebigdream@gmail.com>
2020-03-09 20:09:17 +0900

    Change models.py singleton -> singletion x

--- user/models.py
+++ user/models.py
@@ -1,53 +1,11 @@
 from typing import Any
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=150)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    create_date = models.DateTimeField(auto_now_add=True)
-    verification = models.BooleanField(null=False, default=False)
-    verification_key = models.CharField(max_length=128)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
-
-    def __str__(self) -> str:
-        return self.name
-
-    def has_perm(self, perm: Any, obj: Any = None) -> bool:
-
-        return True
-
-    def has_module_perms(self, app_label: Any) -> bool:
-
-        return True
-
-    def __init__(self, **kwargs: Any) -> None:
-        super().__init__(**kwargs)
-        self.objects = UserManager.get_instance()
-
-
 class UserManager(BaseUserManager):  # type: ignore
-    __instance = None
-
-    @staticmethod
-    def get_instance() -> Any:
-        """ Static access method. """
-        if UserManager.__instance is None:
-            UserManager()
-        return UserManager.__instance
-
-    def __init__(self) -> None:
-        """ Virtually private constructor. """
-        if UserManager.__instance is not None:
-            assert UserManager.__instance is not None, ("UserManager is singleton")
-        else:
-            UserManager.__instance = self
 
     def create_user(self, email: str, name: str,
                     password: str = None) -> 'User':
@@ -75,3 +33,29 @@ class UserManager(BaseUserManager):  # type: ignore
         user.is_superuser = True
         user.save(using=self.db)
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    verification = models.BooleanField(null=False, default=False)
+    verification_key = models.CharField(max_length=128)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.name
+
+    def has_perm(self, perm: Any, obj: Any = None) -> bool:
+
+        return True
+
+    def has_module_perms(self, app_label: Any) -> bool:
+
+        return True
