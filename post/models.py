470a7f9abf3a766b1f418fa9687e719ede4ee672
choitaehoon1998 <hugebigdream@gmail.com>
2020-03-10 23:41:47 +0900

    change models.py set_null , null=true

--- post/models.py
+++ post/models.py
@@ -33,8 +33,8 @@ class Post(models.Model):
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField('Tag')
 
     def __str__(self) -> str:
@@ -51,9 +51,9 @@ class Comment(models.Model):
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
-    post = models.ForeignKey('Post', on_delete=models.PROTECT)
+    post = models.ForeignKey('Post', on_delete=models.SET_NULL, null=True)
     parent_comment = models.ForeignKey('self', on_delete=models.PROTECT, null=True)
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self) -> str:
         return f'Comment(id={self.id})'
