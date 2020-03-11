e70d03e0c8c69aa3aaffaf2d9ce51f33f441ad91
yimsb01 <yimsb02@naver.com>
2020-03-11 17:36:54 +0900

    Add fixtures into the conftest module for comment tests

--- tests/post_test/conftest.py
+++ tests/post_test/conftest.py
@@ -4,7 +4,7 @@ from model_mommy import mommy
 import pytest
 from rest_framework.test import APIClient
 
-from post.models import Category, Post, Tag
+from post.models import Category, Comment, Post, Tag
 from user.models import User
 
 
@@ -30,12 +30,22 @@ def post_id() -> int:
 
 
 @pytest.fixture
+def comment_id() -> int:
+    return 1
+
+
+@pytest.fixture
 def title_data() -> dict:
     return {'title': 'test_title'}
 
 
 @pytest.fixture
-def bad_data() -> dict:
+def content_data() -> dict:
+    return {'content': 'test_content'}
+
+
+@pytest.fixture
+def unauthorised_data() -> dict:
     return {'id': 1, 'author': 65535}
 
 
@@ -51,6 +61,42 @@ def post_data() -> dict:
         'author': 1,
         'category': 1,
         'tags': [1, 2],
+    }
+    return data
+
+
+@pytest.fixture
+def bad_post_data() -> dict:
+    data: dict[str, Any] = {
+        'title': 'test_title',
+        # 'content': 'test_content',
+        'location': 'test_location',
+        'capacity': 10,
+        # 'date': '2020-01-01',
+        'time_of_day': 1,
+        'author': 1,
+        # 'category': 1,
+        # 'tags': [1, 2],
+    }
+    return data
+
+
+@pytest.fixture
+def comment_data() -> dict:
+    data: dict[str, Any] = {
+        'content': 'test_content',
+        'post': 1,
+        'author': 1,
+    }
+    return data
+
+
+@pytest.fixture
+def bad_comment_data() -> dict:
+    data: dict[str, Any] = {
+        # 'content': 'test_content',
+        'post': 1,
+        # 'author': 1,
     }
     return data
 
@@ -73,3 +119,8 @@ def tags() -> List[Tag]:
 @pytest.fixture
 def posts() -> List[Post]:
     return [mommy.make(Post) for i in range(3)]
+
+
+@pytest.fixture
+def comments() -> List[Comment]:
+    return [mommy.make(Comment) for i in range(3)]
