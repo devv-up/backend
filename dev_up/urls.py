1b5907d6ef0675eed6d36d7e2a535d19b45e8eba
yimsb01 <yimsb02@naver.com>
2020-03-15 00:11:51 +0900

    Remove redundancy
    As the default value of permission_classes is permissions.AllowAny,
    removed the line that specify permission classes on default swagger urls.
    
    If `permissions.AllowAny` is set, it will cause a mypy error
    : error: Argument "permission_classes" to "get_schema_view" has incompatible type
      "Tuple[Type[AllowAny]]"; expected "Optional[Tuple[str]]"
    
    This, `Optional[Tuple[str]], means: str 'rest_framework.permissions.AllowAny'
    in settings.py of `rest_framework` that is redundancy.

--- dev_up/urls.py
+++ dev_up/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import permissions  # type: ignore
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -26,7 +25,6 @@ schema_view = get_schema_view(
         description="A simple instruction manual for those who need the dev-up api.",
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,)
 )
 
 urlpatterns = [
