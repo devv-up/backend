5b42956e9317ca79190fbd106d2ba1c669c3f1f6
choitaehoon1998 <hugebigdream@gmail.com>
2020-03-19 04:55:22 +0900

    Change routers.py and setup.cfg

--- user/routers.py
+++ user/routers.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
-from rest_framework_jwt.views import (obtain_jwt_token, refresh_jwt_token,
-                                      verify_jwt_token)
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
--- user/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from user.models import User
-
-
-class UserRegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email', 'name', 'password')
