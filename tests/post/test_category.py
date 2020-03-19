b0c6d2cc1fa1c25f3f0eaa5f2b25f3b1f07ae643
yimsb01 <yimsb02@naver.com>
2020-03-19 21:36:38 +0900

    Fix uris in tests

--- tests/post/test_category.py
+++ tests/post/test_category.py
@@ -6,39 +6,39 @@ class TestCategory:
     pytestmark = pytest.mark.django_db
 
     def test_create_category(self, api_client, title_data):
-        response = api_client.post('/categories', title_data)
+        response = api_client.post('/posts/categories', title_data)
         assert response.status_code == 201
 
     def test_create_the_same_title_category(self, api_client, title_data):
-        response1 = api_client.post('/categories', title_data)
-        response2 = api_client.post('/categories', title_data)
+        response1 = api_client.post('/posts/categories', title_data)
+        response2 = api_client.post('/posts/categories', title_data)
 
         assert response1.status_code == 201
         assert response2.status_code == 400
 
     def test_create_category_without_title(self, api_client):
-        response = api_client.post('/categories')
+        response = api_client.post('/posts/categories')
         assert response.status_code == 400
 
     def test_list_categories(self, api_client, categories):
-        response = api_client.get('/categories')
+        response = api_client.get('/posts/categories')
 
         assert response.status_code == 200
         assert len(response.data) > 1
 
     def test_detail_category(self, api_client, categories, dummy_id):
-        response = api_client.get(f'/categories/{dummy_id}')
+        response = api_client.get(f'/posts/categories/{dummy_id}')
 
         assert response.status_code == 200
         assert response.data.get('id') == dummy_id
 
     def test_detail_missing_category(self, api_client):
-        response = api_client.get('/categories/65535')
+        response = api_client.get('/posts/categories/65535')
         assert response.status_code == 404
 
     def test_update_category(self, api_client, categories, dummy_id, title_data):
-        before_update = api_client.get(f'/categories/{dummy_id}')
-        response = api_client.put(f'/categories/{dummy_id}', data=title_data)
+        before_update = api_client.get(f'/posts/categories/{dummy_id}')
+        response = api_client.put(f'/posts/categories/{dummy_id}', data=title_data)
 
         assert response.status_code == 201
         assert response.data.get('id') == dummy_id
@@ -46,9 +46,9 @@ class TestCategory:
         assert before_update.data.get('title') != title_data.get('title')
 
     def test_delete_category(self, api_client, categories, dummy_id):
-        before_delete = api_client.get(f'/categories/{dummy_id}')
-        response = api_client.delete(f'/categories/{dummy_id}')
-        after_delete = api_client.get(f'/categories/{dummy_id}')
+        before_delete = api_client.get(f'/posts/categories/{dummy_id}')
+        response = api_client.delete(f'/posts/categories/{dummy_id}')
+        after_delete = api_client.get(f'/posts/categories/{dummy_id}')
 
         assert before_delete.status_code == 200
         assert response.status_code == 204
