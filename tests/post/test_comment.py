b138188163832639c1a674fa869dc0f77ace528a
yimsb01 <yimsb02@naver.com>
2020-03-19 21:36:38 +0900

    Fix uris in tests

--- tests/post/test_comment.py
+++ tests/post/test_comment.py
@@ -6,32 +6,32 @@ class TestComment:
     pytestmark = pytest.mark.django_db
 
     def test_create_comment(self, api_client, users, posts, comment_data):
-        response = api_client.post('/comments', comment_data)
+        response = api_client.post('/posts/comments', comment_data)
         assert response.status_code == 201
 
     def test_create_comment_without_post_no(self, api_client, users, bad_comment_data):
-        response = api_client.post('/comments', bad_comment_data)
+        response = api_client.post('/posts/comments', bad_comment_data)
         assert response.status_code == 400
 
     def test_list_commments(self, api_client, comments):
-        response = api_client.get('/comments')
+        response = api_client.get('/posts/comments')
 
         assert response.status_code == 200
         assert len(response.data) > 1
 
     def test_detail_comments(self, api_client, comments, dummy_id):
-        response = api_client.get(f'/comments/{dummy_id}')
+        response = api_client.get(f'/posts/comments/{dummy_id}')
 
         assert response.status_code == 200
         assert response.data.get('id') == dummy_id
 
     def test_detail_missing_comment(self, api_client):
-        response = api_client.get('/comments/65535')
+        response = api_client.get('/posts/comments/65535')
         assert response.status_code == 404
 
     def test_update_comment(self, api_client, comments, dummy_id, content_data):
-        before_update = api_client.get(f'/comments/{dummy_id}')
-        response = api_client.put(f'/comments/{dummy_id}', data=content_data)
+        before_update = api_client.get(f'/posts/comments/{dummy_id}')
+        response = api_client.put(f'/posts/comments/{dummy_id}', data=content_data)
 
         assert response.status_code == 201
         assert response.data.get('id') == dummy_id
@@ -39,17 +39,17 @@ class TestComment:
         assert before_update.data.get('content') != content_data.get('content')
 
     def test_update_comment_with_bad_access(self, api_client, comments, dummy_id, bad_post_data):
-        response = api_client.put(f'/comments/{dummy_id}', data=bad_post_data)
+        response = api_client.put(f'/posts/comments/{dummy_id}', data=bad_post_data)
         assert response.status_code == 403
 
     def test_update_parent_comment(self, api_client, comments, dummy_id):
-        response = api_client.put(f'/comments/{dummy_id}', data={"parent_comment": 65535})
+        response = api_client.put(f'/posts/comments/{dummy_id}', data={"parent_comment": 65535})
         assert response.status_code == 403
 
     def test_delete_post(self, api_client, comments, dummy_id):
-        before_delete = api_client.get(f'/comments/{dummy_id}')
-        response = api_client.delete(f'/comments/{dummy_id}')
-        after_delete = api_client.get(f'/comments/{dummy_id}')
+        before_delete = api_client.get(f'/posts/comments/{dummy_id}')
+        response = api_client.delete(f'/posts/comments/{dummy_id}')
+        after_delete = api_client.get(f'/posts/comments/{dummy_id}')
 
         assert before_delete.status_code == 200
         assert response.status_code == 204
--- tests/post/test_tag.py
+++ tests/post/test_tag.py
@@ -6,44 +6,44 @@ class TestTag:
     pytestmark = pytest.mark.django_db
 
     def test_create_tag(self, api_client, title_data):
-        response = api_client.post('/tags', title_data)
+        response = api_client.post('/posts/tags', title_data)
         assert response.status_code == 201
 
     def test_create_the_same_title_tag(self, api_client, title_data):
-        response1 = api_client.post('/tags', title_data)
-        response2 = api_client.post('/tags', title_data)
+        response1 = api_client.post('/posts/tags', title_data)
+        response2 = api_client.post('/posts/tags', title_data)
 
         assert response1.status_code == 201
         assert response2.status_code == 400
 
     def test_create_tag_without_title(self, api_client):
-        response = api_client.post('/tags')
+        response = api_client.post('/posts/tags')
         assert response.status_code == 400
 
     def test_list_tags(self, api_client, tags):
-        response = api_client.get('/tags')
+        response = api_client.get('/posts/tags')
 
         assert response.status_code == 200
         assert len(response.data) > 1
 
     def test_detail_tag(self, api_client, tags, dummy_id):
-        response = api_client.get(f'/tags/{dummy_id}')
+        response = api_client.get(f'/posts/tags/{dummy_id}')
 
         assert response.status_code == 200
         assert response.data.get('id') == dummy_id
 
     def test_detail_missing_tag(self, api_client):
-        response = api_client.get('/tags/65535')
+        response = api_client.get('/posts/tags/65535')
         assert response.status_code == 404
 
     def test_update_tag_request(self, api_client, tags, dummy_id, title_data):
-        response = api_client.put(f'/tags/{dummy_id}', data=title_data)
+        response = api_client.put(f'/posts/tags/{dummy_id}', data=title_data)
         assert response.status_code == 405
 
     def test_delete_tag(self, api_client, tags, dummy_id):
-        before_delete = api_client.get(f'/tags/{dummy_id}')
-        response = api_client.delete(f'/tags/{dummy_id}')
-        after_delete = api_client.get(f'/tags/{dummy_id}')
+        before_delete = api_client.get(f'/posts/tags/{dummy_id}')
+        response = api_client.delete(f'/posts/tags/{dummy_id}')
+        after_delete = api_client.get(f'/posts/tags/{dummy_id}')
 
         assert before_delete.status_code == 200
         assert response.status_code == 204
