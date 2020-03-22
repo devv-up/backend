35d2a01a067358bf7f7431f068bb7d63208565d6
yimsb01 <yimsb02@naver.com>
2020-03-23 07:21:48 +0900

    Remove all http methods except for `get()` and polish codes
    Change Log:
    - Tag and Category API now have only get method.
    - Optional parameter `url_resources` are removed.

--- post/api/tag.py
+++ post/api/tag.py
@@ -1,7 +1,3 @@
-from typing import Optional
-
-from rest_framework import status
-from rest_framework.exceptions import ParseError
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,15 +7,13 @@ from post.serializers import TagSerializer
 
 
 class TagAPI(APIView):
-    def get(self, request: Request, **url_resources: Optional[int]) -> Response:
+    def get(self, request: Request, tag_id: int = None) -> Response:
         """
-        Gets a tag object or a list of tags.
-        Basically, this function returns a response that include data of
-        whole tags unless a specific tag id is given
+        Get a tag object or the list of tags.
+        Basically, this function returns a response that includes data of
+        whole tags unless a specific tag ID is given
         by uri resources.
         """
-        tag_id = url_resources.get('tag_id')
-
         if tag_id:
             tag = APIUtils.get('Tag', id=tag_id)
             serializer = TagSerializer(tag)
@@ -28,25 +22,3 @@ class TagAPI(APIView):
             tags = APIUtils.get_list_of('Tag')
             serializer = TagSerializer(tags, many=True)
             return Response(serializer.data)
-
-    def post(self, request: Request) -> Response:
-        """
-        Creates a tag.
-        """
-        serializer = TagSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        raise ParseError(detail=serializer.errors)
-
-    def delete(self, request: Request, tag_id: int) -> Response:
-        """
-        Delete the tag.
-        The specific tag ID must be required by uri resources.
-        """
-        tag = APIUtils.get('Tag', id=tag_id)
-        tag.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
