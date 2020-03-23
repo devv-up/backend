b71778d611c76c9a0efce1895f74b2244b128eab
yimsb01 <yimsb02@naver.com>
2020-03-23 10:22:19 +0900

    Remove detail function related test codes

--- tests/post/test_tag.py
+++ tests/post/test_tag.py
@@ -8,12 +8,3 @@ class TestTag:
         response = api_client.get('/posts/tags')
         assert response.status_code == 200
         assert len(response.data) > 1
-
-    def test_detail_tag(self, api_client, tags):
-        response = api_client.get('/posts/tags/1')
-        assert response.status_code == 200
-        assert response.data['id'] == 1
-
-        # Get a tag that doesn't exist.
-        response = api_client.get('/posts/tags/65535')
-        assert response.status_code == 404
