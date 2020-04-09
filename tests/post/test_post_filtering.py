01603bc31473c0d1087380799840c6c576656d94
yimsb01 <yimsb02@naver.com>
2020-04-09 12:10:34 +0900

    Modify test codes
    Change Log:
    - Query parameter `page` is now mandatory.
      Hence, testing `get list of posts without page no.` is removed
    - All the util functions such as `count tags of` are moved outside of Test class and
      modified visibility.

--- tests/post/test_post_filtering.py
+++ tests/post/test_post_filtering.py
@@ -42,71 +42,74 @@ def posts(categories, tags):
     return [post1, post2, post3]
 
 
+def count_tags_of(result, *title_filters):
+    tags = [tag for tag in result['tags'] if tag['title'] in title_filters]
+    return len(tags)
+
+
+def count_tags_of_multiple(response, *tag_titles):
+    for result in response.data:
+        number_of_tags = count_tags_of(result, *tag_titles)
+        assert number_of_tags == len(tag_titles)
+
+
+def check_data_duplication_of(response):
+    ids = set()
+    for result in response.data:
+        ids.add(result['id'])
+    assert len(ids) == len(response.data)
+
+
 class TestPostFiltering:
     pytestmark = pytest.mark.django_db
 
-    def __count_tags_of(self, result, *title_filters):
-        tags = [tag for tag in result['tags'] if tag['title'] in title_filters]
-        return len(tags)
-
-    def __count_tags_of_multiple(self, response, *tag_titles):
-        for result in response.data:
-            number_of_tags = self.__count_tags_of(result, *tag_titles)
-            assert number_of_tags == len(tag_titles)
-
-    def __check_data_duplication_of(self, response):
-        ids = set()
-        for result in response.data:
-            ids.add(result['id'])
-        assert len(ids) == len(response.data)
-
     def test_post_on_category_filtering(self, api_client, posts, categories):
         category_title = categories[0].title
-        response = api_client.get(f'/posts?category={category_title}')
+        response = api_client.get(f'/posts?category={category_title}&page=1')
         assert response.status_code == 200
         assert len(response.data) == 2
 
-        self.__check_data_duplication_of(response)
+        check_data_duplication_of(response)
         for data in response.data:
             assert data['category']['title'] == category_title
 
     def test_post_on_tag_filtering(self, api_client, posts, tags):
         tag_title = tags[1].title
-        response = api_client.get(f'/posts?tags={tag_title}')
+        response = api_client.get(f'/posts?tags={tag_title}&page=1')
         assert response.status_code == 200
         assert len(response.data) == 2
 
-        self.__check_data_duplication_of(response)
-        self.__count_tags_of_multiple(response, tag_title)
+        check_data_duplication_of(response)
+        count_tags_of_multiple(response, tag_title)
 
         # Filter the posts by more than one tag.
         tag_title1, tag_title2 = tags[0].title, tags[1].title
-        response = api_client.get(f'/posts?tags={tag_title1},{tag_title2}')
+        response = api_client.get(f'/posts?tags={tag_title1},{tag_title2}&page=1')
         assert response.status_code == 200
         assert len(response.data) == 2
 
-        self.__check_data_duplication_of(response)
-        self.__count_tags_of_multiple(response, tag_title1, tag_title2)
+        check_data_duplication_of(response)
+        count_tags_of_multiple(response, tag_title1, tag_title2)
 
         # This type of tag filtering is not supported.
-        response = api_client.get(f'/posts?tags={tag_title1}&tags={tag_title2}')
+        response = api_client.get(f'/posts?tags={tag_title1}&tags={tag_title2}&page=1')
         assert response.status_code == 400
 
     def test_post_on_category_tag_filtering(self, api_client, posts, categories, tags):
         category_title, tag_title = categories[0].title, tags[1].title
-        response = api_client.get(f'/posts?category={category_title}&tags={tag_title}')
+        response = api_client.get(f'/posts?category={category_title}&tags={tag_title}&page=1')
         assert response.status_code == 200
         assert len(response.data) == 1
 
         result = response.data[0]
         assert category_title == result['category']['title']
 
-        number_of_tags = self.__count_tags_of(result, tag_title)
+        number_of_tags = count_tags_of(result, tag_title)
         assert number_of_tags == 1
 
     def test_post_on_date_filtering(self, api_client, posts):
         start_date, end_date = '2020-01-01', '2020-02-02'
-        response = api_client.get(f'/posts?startDate={start_date}&endDate={end_date}')
+        response = api_client.get(f'/posts?startDate={start_date}&endDate={end_date}&page=1')
         assert response.status_code == 200
         assert len(response.data) == 2
 
@@ -115,7 +118,7 @@ class TestPostFiltering:
 
     def test_post_on_time_of_day_filtering(self, api_client, posts):
         time_of_day = TIME_OF_DAY['AFTERNOON']
-        response = api_client.get(f'/posts?timeOfDay={time_of_day}')
+        response = api_client.get(f'/posts?timeOfDay={time_of_day}&page=1')
         assert response.status_code == 200
         assert len(response.data) == 2
 
@@ -124,7 +127,7 @@ class TestPostFiltering:
 
     def test_post_on_location_filtering(self, api_client, posts):
         location = 'location'
-        response = api_client.get(f'/posts?location={location}')
+        response = api_client.get(f'/posts?location={location}&page=1')
         assert response.status_code == 200
         assert len(response.data) == 2
 
