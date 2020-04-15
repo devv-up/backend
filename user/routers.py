095309c9bedf8b941bc66b62bddac950801c6886
choitaehoon1998 <hugebigdream@gmail.com>
2020-04-16 04:06:46 +0900

    [ADD] jwt user
    sign up -> email , login out -> jwt

--- user/routers.py
+++ user/routers.py
@@ -13,12 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from allauth.account.views import confirm_email
 from django.urls import include, path
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from rest_framework.compat import re_path
+
+from user.social import GithubConnect, GithubLogin, GoogleConnect, GoogleLogin
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
-    path('api-token-auth/', obtain_jwt_token),
-    path('api-token-refresh/', refresh_jwt_token),
-    path('api-token-verify/', verify_jwt_token),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('rest-auth/github/', GithubLogin.as_view(), name='github_login'),
+    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('rest-auth/github/connect', GithubConnect.as_view(), name='github_connect'),
+    path('rest-auth/google/connect', GoogleConnect.as_view(), name='google_connect'),
+    re_path('accounts-rest/registration/account-confirm-email/(?P<key>.+)/$',
+            confirm_email, name='account_confirm_email'),
 ]
