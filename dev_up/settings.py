624fb72c8aa0701226443b98f6f3fff14c7b7ab8
choitaehoon1998 <hugebigdream@gmail.com>
2020-04-27 22:14:16 +0900

    Change what was pointed

--- dev_up/settings.py
+++ dev_up/settings.py
@@ -138,11 +138,11 @@ AUTHENTICATION_BACKENDS = (
 
 WSGI_APPLICATION = 'dev_up.wsgi.application'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", 'django.core.mail.backends.smtp.EmailBackend'),
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", '')
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", '')
-EMAIL_PORT = 587
+EMAIL_PORT = os.environ.get("EMAIL_PORT", 587),
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
