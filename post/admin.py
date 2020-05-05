402e721c378e5f33cd272a9d819b25d641acaa5b
yimsb01 <yimsb02@naver.com>
2020-05-05 09:36:15 +0900

    Create Like model and register

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from post.models import Category, Comment, Post, Tag
+from post.models import Category, Comment, Like, Post, Tag
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Tag)
+admin.site.register(Like)
