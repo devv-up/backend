fee87c9304a8a91d806f48ec4af159256ad7f1de
yimsb01 <yimsb02@naver.com>
2020-06-27 22:11:07 +0900

    Change jwt module
    The update on Feb 2020 of Google Chrome sets
    headers `SameSite=Lax` if they are not set `SameSite`
    that block storing jwt cookie.
    
    That is why JWT_AUTH_SAMESITE=None
    
    As rest-auth is not maintained anymore django-rest-auth
    is changed to dj-rest-auth and djangorestframework-simplejwt is
    also applied because dj-rest-auth supports it.

--- user/social.py
+++ user/social.py
@@ -4,7 +4,7 @@ import os
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from rest_auth.registration.views import SocialLoginView
+from dj_rest_auth.registration.views import SocialLoginView
 
 SITE_URL = os.environ.get("SITE_URL", "http://127.0.0.1:8000")
 
