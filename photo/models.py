71fa13c83292962006dc132859c64c6bc94f5b3e
choitaehoon1998 <hugebidream@gmail.com>
2020-06-28 13:27:40 +0900

    Change models.py

--- photo/models.py
+++ photo/models.py
@@ -6,8 +6,7 @@ class photo(models.Model):
     photo_category = models.CharField(max_length=30)
     photo_detail = models.CharField(max_length=30)
     # set default to anything or blank image
-    image = models.ImageField(max_length=None, upload_to="images/%Y/%m/%d",
-                              default='images/None/no-img,jpg')
+    image = models.ImageField(upload_to="images/%Y/%m/%d", default='images/None/no-img,jpg')
 
     def __str__(self) -> str:
         return f'Photo(category="{self.photo_category}", detail="{self.photo_detail}")'
